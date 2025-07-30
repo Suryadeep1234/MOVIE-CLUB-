5c07c510253c943c05d22e0ab4b8b5673606dad5
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-07-30 11:36:41 +0530

    Update pmfilter (3).py

--- plugins/pmfilter (3).py
+++ plugins/pmfilter (3).py
@@ -170,12 +170,15 @@ async def next_page(bot, query):
     settings = await get_settings(query.message.chat.id)
     if settings.get('button'):
         btn = [
-            [
-                InlineKeyboardButton(text=f"🔗 {get_size(file.file_size)} ≽ " + clean_filename(
-                    file.file_name), callback_data=f'file#{file.file_id}'),
-            ]
-            for file in files
-        ]
+    [
+        InlineKeyboardButton(
+            text=f"[{get_size(file.file_size)}] {truncate_filename(clean_filename(file.file_name))}",
+            callback_data=f"file#{file.file_id}"
+        )
+    ]
+    for file in files
+]
+
         btn.insert(0,
                    [
                        InlineKeyboardButton(
@@ -407,12 +410,15 @@ async def filter_qualities_cb_handler(client: Client, query: CallbackQuery):
     settings = await get_settings(message.chat.id)
     if settings.get('button'):
         btn = [
-            [
-                InlineKeyboardButton(text=f"🔗 {get_size(file.file_size)} ≽ " + clean_filename(
-                    file.file_name), callback_data=f'file#{file.file_id}'),
-            ]
-            for file in files
-        ]
+    [
+        InlineKeyboardButton(
+            text=f"[{get_size(file.file_size)}] {truncate_filename(clean_filename(file.file_name))}",
+            callback_data=f"file#{file.file_id}"
+        )
+    ]
+    for file in files
+]
+
         btn.insert(0,
                    [
                        InlineKeyboardButton(
@@ -565,12 +571,15 @@ async def filter_languages_cb_handler(client: Client, query: CallbackQuery):
     settings = await get_settings(message.chat.id)
     if settings.get('button'):
         btn = [
-            [
-                InlineKeyboardButton(text=f"🔗 {get_size(file.file_size)} ≽ " + clean_filename(
-                    file.file_name), callback_data=f'file#{file.file_id}'),
-            ]
-            for file in files
-        ]
+    [
+        InlineKeyboardButton(
+            text=f"[{get_size(file.file_size)}] {truncate_filename(clean_filename(file.file_name))}",
+            callback_data=f"file#{file.file_id}"
+        )
+    ]
+    for file in files
+]
+
         btn.insert(0,
                    [
                        InlineKeyboardButton(
@@ -715,15 +724,14 @@ async def filter_seasons_cb_handler(client: Client, query: CallbackQuery):
     if settings.get("button"):
         btn.extend(
             [
-                [
-                    InlineKeyboardButton(
-                        f"🔗 {get_size(f.file_size)} ≽ " +
-                        clean_filename(f.file_name),
-                        callback_data=f"file#{f.file_id}",
-                    )
-                ]
-                for f in files
-            ]
+    [
+        InlineKeyboardButton(
+            text=f"[{get_size(file.file_size)}] {truncate_filename(clean_filename(file.file_name))}",
+            callback_data=f"file#{file.file_id}"
+        )
+    ]
+    for file in files
+]
         )
     btn.insert(
         0,
@@ -1909,12 +1917,15 @@ async def auto_filter(client, msg, spoll=False):
     temp.SHORT[message.from_user.id] = message.chat.id
     if settings.get('button'):
         btn = [
-            [
-                InlineKeyboardButton(text=f"🔗 {get_size(file.file_size)} ≽ " + clean_filename(
-                    file.file_name), callback_data=f'file#{file.file_id}'),
-            ]
-            for file in files
-        ]
+    [
+        InlineKeyboardButton(
+            text=f"[{get_size(file.file_size)}] {truncate_filename(clean_filename(file.file_name))}",
+            callback_data=f"file#{file.file_id}"
+        )
+    ]
+    for file in files
+]
+
         btn.insert(0,
                    [
                        InlineKeyboardButton(
