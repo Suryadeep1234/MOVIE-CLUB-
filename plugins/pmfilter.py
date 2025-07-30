2a1bd68b4e0be8669f6b7340e136c37b9d578235
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-07-30 21:38:52 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1860,6 +1860,7 @@ async def auto_filter(client, msg, spoll=False):
         if len(message.text) < 100:
             search = message.text
             search = search.lower()
+            user_name = message.from_user.first_name  # ✅ get user's first name
             m = await message.reply_text(f'**Wᴀɪᴛ Sᴇᴀʀᴄʜɪɴɢ Yᴏᴜʀ Qᴜᴇʀʏ :** `{search}...`', reply_to_message_id=message.id)
             find = search.split(" ")
             search = ""
@@ -1895,6 +1896,7 @@ async def auto_filter(client, msg, spoll=False):
     else:
         message = msg.message.reply_to_message
         search, files, offset, total_results = spoll
+        user_name = message.from_user.first_name  # ✅ get user's first name
         m = await message.reply_text(f'**Wᴀɪᴛ Sᴇᴀʀᴄʜɪɴɢ Yᴏᴜʀ Qᴜᴇʀʏ :** `{search}...`', reply_to_message_id=message.id)
         settings = await get_settings(message.chat.id)
         await msg.message.delete()
