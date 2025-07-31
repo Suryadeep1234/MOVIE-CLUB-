fd6d97de0498101506e838245048e9c1405055b2
Suryadeep1234 <gm773328@gmail.com>
2025-07-31 23:00:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,8 +73,8 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://subhadeeprampur: subhadeeprampur@subhadeep.hg7u8qy.mongodb.net/subhadeeprampur?retryWrites=true&w=majority&appName=Subhadeep")  # MongoDB URI for the database
-DATABASE_NAME = environ.get('DATABASE_NAME', "subhadeeprampur") # Database name (default: cluster)
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://RDX:RDX@cluster0.ckt1p.mongodb.net/RDX?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB URI for the database
+DATABASE_NAME = environ.get('DATABASE_NAME', "RDX") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collection name (default: dreamcinezone_files)
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
