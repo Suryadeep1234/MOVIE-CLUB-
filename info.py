9bb79de8d44740ea1e10ad3111abd94881376a8d
Suryadeep1234 <gm773328@gmail.com>
2025-08-31 19:34:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -123,7 +123,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/MC_MOVIES_PVT') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/RDX1444') # Owner link for the bot
-UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/MC_MOVIES_PVT') # Update channel link for the bot
+UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/MC_PVT_HD') # Update channel link for the bot
 
 # ============================
 # User Configuration
