ccb11385943d529229f59ae3f8ba570f41b6a936
Suryadeep1234 <gm773328@gmail.com>
2025-11-05 09:43:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,3 +99,7 @@ if __name__ == '__main__':
         except KeyboardInterrupt:
             logging.info('Service Stopped Bye 👋')
             break
+import os
+
+port = int(os.environ.get("PORT", 8080))
+app.run(host="0.0.0.0", port=port)
