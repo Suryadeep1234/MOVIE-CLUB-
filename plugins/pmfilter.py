ecde4afcd1a648480616f5bc74db340de45a546a
Suryadeep1234 <gm773328@gmail.com>
2025-11-10 22:52:13 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2024,114 +2024,70 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
         temp.IMDB_CAP[message.from_user.id] = cap
+            if not settings.get('button'):
+                cap += "\n\n<b><u>Your Requested Files Are Here</u></b>\n\n"
+                for idx, file in enumerate(files, start=1):
+                    cap += f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>[{get_size(file.file_size)}] {clean_filename(file.file_name)}\n</a></b>"
+        else:
+            temp.IMDB_CAP[message.from_user.id] = None
+            if ULTRA_FAST_MODE:
+                if settings.get('button'):
+                    cap = f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n\n📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}\n⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {message.chat.title or temp.B_LINK or 'ᴅʀᴇᴀᴍxʙᴏᴛᴢ'} \n\n<u>Your Requested Files Are Here</u> \n\n</b>"
+                else:
+                    cap = f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n\n📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}\n⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {message.chat.title or temp.B_LINK or 'ᴅʀᴇᴀᴍxʙᴏᴛᴢ'} \n\n<u>Your Requested Files Are Here</u> \n\n</b>"
+                    for idx, file in enumerate(files, start=1):
+                        cap += f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>[{get_size(file.file_size)}] {clean_filename(file.file_name)}\n</a></b>"
+            else:
+                if settings.get('button'):
+                    cap = f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n🧱 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n\n📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}\n⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {message.chat.title or temp.B_LINK or 'ᴅʀᴇᴀᴍxʙᴏᴛᴢ'} \n\n<u>Your Requested Files Are Here</u> \n\n</b>"
+                else:
+                    cap = f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n🧱 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n\n📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}\n⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {message.chat.title or temp.B_LINK or 'ᴅʀᴇᴀᴍxʙᴏᴛᴢ'} \n\n<u>Your Requested Files Are Here</u> \n\n</b>"
 
-if not settings.get('button'):
-    cap += "\n\n<b>🧾 <u>Your Requested Files Are Here</u> 👇</b>"
-    for idx, file in enumerate(files, start=1):
-        cap += (
-            f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>"
-            f"[{get_size(file.file_size)}] {clean_filename(file.file_name)}\n</a></b>"
-        )
-else:
-    if settings.get('button'):
-        cap = (
-            f"<b>Query:</b> <b>{query}</b>\n\n"
-            f"<b>IMDb Data:</b>\n\n"
-            f"<b>🏷 Title:</b> <b><a href=\"{url}\">{title}</a></b><br>"
-            f"<b>🎭 Genres:</b> <b>{genres}</b><br>"
-            f"<b>📆 Year:</b> <b><a href=\"{url}/releaseinfo\">{year}</a></b><br>"
-            f"<b>🌟 Rating:</b> <b><a href=\"{url}/ratings\">{rating}</a></b> <b>/ 10 (based on {votes} user ratings)</b><br>"
-            f"<b>☀️ Languages:</b> <b><code>{languages}</code></b><br>"
-            f"<b>📀 RunTime:</b> <b>{runtime} Minutes</b><br>"
-            f"<b>📆 Release Info:</b> <b>{release_date}</b><br>"
-            f"<b>🎛 Countries:</b> <b><code>{countries}</code></b><br><br>"
-            f"<b>⏰ Result Shown in:</b> <b><i>{remaining_seconds}</i> seconds</b> 🔥<br><br>"
-            f"<b>Requested by:</b> <b>{message.from_user.mention}</b>"
-        )
-    else:
-        cap = (
-            f"<b>Query:</b> <b>{query}</b>\n\n"
-            f"<b>IMDb Data:</b>\n\n"
-            f"<b>🏷 Title:</b> <b><a href=\"{url}\">{title}</a></b><br>"
-            f"<b>🎭 Genres:</b> <b>{genres}</b><br>"
-            f"<b>📆 Year:</b> <b><a href=\"{url}/releaseinfo\">{year}</a></b><br>"
-            f"<b>🌟 Rating:</b> <b><a href=\"{url}/ratings\">{rating}</a></b> <b>/ 10 (based on {votes} user ratings)</b><br>"
-            f"<b>☀️ Languages:</b> <b><code>{languages}</code></b><br>"
-            f"<b>📀 RunTime:</b> <b>{runtime} Minutes</b><br>"
-            f"<b>📆 Release Info:</b> <b>{release_date}</b><br>"
-            f"<b>🎛 Countries:</b> <b><code>{countries}</code></b><br><br>"
-            f"<b>⏰ Result Shown in:</b> <b><i>{remaining_seconds}</i> seconds</b> 🔥<br><br>"
-            f"<b>Requested by:</b> <b>{message.from_user.mention}</b>"
-        )
-        cap = "\n\n<b>🧾 <u>Your Requested Files Are Here</u> 👇</b>"
-    for idx, file in enumerate(files, start=1):
-        cap += (
-            f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>"
-            f"[{get_size(file.file_size)}] {clean_filename(file.file_name)}\n</a></b>"
-        )
+                    for idx, file in enumerate(files, start=1):
+                        cap += f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>[{get_size(file.file_size)}] {clean_filename(file.file_name)}\n</a></b>"
 
-    if imdb and imdb.get('poster'):
+        sent = None
         try:
-            hehe = await message.reply_photo(
-                photo=imdb.get('poster'),
-                caption=cap,
-                reply_markup=InlineKeyboardMarkup(btn),
-                parse_mode=enums.ParseMode.HTML
-            )
-            await m.delete()
-
-            try:
-                if settings['auto_delete']:
-                    await asyncio.sleep(DELETE_TIME)
-                    await hehe.delete()
-                    await message.delete()
-            except KeyError:
-                await save_group_settings(message.chat.id, 'auto_delete', True)
-                await asyncio.sleep(DELETE_TIME)
-                await hehe.delete()
-                await message.delete()
-        except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-            pic = imdb.get('poster')
-            poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            hmm = await message.reply_photo(photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(btn), parse_mode=enums.ParseMode.HTML)
-            await m.delete()
-            try:
-                if settings['auto_delete']:
-                    await asyncio.sleep(DELETE_TIME)
-                    await hmm.delete()
-                    await message.delete()
-            except KeyError:
-                await save_group_settings(message.chat.id, 'auto_delete', True)
-                await asyncio.sleep(DELETE_TIME)
-                await hmm.delete()
-                await message.delete()
+            if imdb and imdb.get('poster'):
+                try:
+                    if TMDB_POSTER:
+                        photo = imdb.get('backdrop') if imdb.get('backdrop') and LANDSCAPE_POSTER else imdb.get('poster')
+                    else:
+                        photo = imdb.get('poster')
+                    sent = await message.reply_photo(photo=photo, caption=cap, reply_markup=InlineKeyboardMarkup(btn), parse_mode=enums.ParseMode.HTML)
+                    if m:
+                        await m.delete()
+                except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+                    pic = imdb.get('poster')
+                    poster = pic.replace('.jpg', "._V1_UX360.jpg")
+                    sent = await message.reply_photo(photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(btn), parse_mode=enums.ParseMode.HTML)
+                    if m:
+                        await m.delete()
+                except Exception as e:
+                    logger.exception(e)
+                    sent = await message.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True, parse_mode=enums.ParseMode.HTML)
+            else:
+                sent = await message.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True, parse_mode=enums.ParseMode.HTML)
+                if m:
+                    await m.delete()
         except Exception as e:
-            logger.exception(e)
-            dxb = await message.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(btn), parse_mode=enums.ParseMode.HTML)
-            try:
-                if settings['auto_delete']:
-                    await asyncio.sleep(DELETE_TIME)
-                    await dxb.delete()
-                    await message.delete()
-            except KeyError:
-                await save_group_settings(message.chat.id, 'auto_delete', True)
-                await asyncio.sleep(DELETE_TIME)
-                await dxb.delete()
-                await message.delete()
-    else:
-        dxb = await message.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True, parse_mode=enums.ParseMode.HTML)
-        await m.delete()
-        try:
-            if settings['auto_delete']:
-                await asyncio.sleep(DELETE_TIME)
-                await dxb.delete()
-                await message.delete()
-        except KeyError:
-            await save_group_settings(message.chat.id, 'auto_delete', True)
-            await asyncio.sleep(DELETE_TIME)
-            await dxb.delete()
-            await message.delete()
+            logger.exception("Failed to send result: %s", e)
+            return
 
+        try:
+            if settings.get('auto_delete'):
+                asyncio.create_task(_schedule_delete(sent, message, DELETE_TIME))
+        except KeyError:
+            try:
+                await save_group_settings(message.chat.id, 'auto_delete', True)
+            except Exception:
+                pass
+            asyncio.create_task(_schedule_delete(sent, message, DELETE_TIME))
+        return
+
+    except Exception as e:
+        logger.exception(e)
+        return
 
 async def ai_spell_check(chat_id, wrong_name):
     async def search_movie(wrong_name):
@@ -2197,4 +2153,4 @@ async def advantage_spell_chok(client, message):
     try:
         await message.delete()
     except:
-        pass
+        pass      
